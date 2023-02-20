4c9f9bcda3c7bbed46221f2322fccc498030906c
Tin Goricki <exibodude@gmail.com>
2023-02-20 21:12:04 +0100

    Made the grid fit in a certain area

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ function createStartingGrid () {
 
     const container = document.querySelector("#container");
 
-    for( let i = 0; i < 16; i++ ) {
-        for( let j = 0; j < 16; j++ ) {
+    for( let i = 0; i < 64; i++ ) {
+        for( let j = 0; j < 64; j++ ) {
 
         const square = document.createElement( "div" );
 
         square.setAttribute( "class", "square" );
 
+        addHoverEffect( square );
+
         container.appendChild( square );
 
         }
     }
+
+}
+
+function addHoverEffect ( square ) {
+
+    square.addEventListener("mouseover", () => {
+
+        square.style.backgroundColor = "black";
+
+    });
 
 }
 
