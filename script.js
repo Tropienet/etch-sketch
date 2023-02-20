3044ee0f14c29c4523f74a236e6a6d048e6e77da
Tin Goricki <exibodude@gmail.com>
2023-02-20 19:13:24 +0100

    Wrote a function to create a starting 16x16 grid

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function createStartingGrid () {
+
+    const container = document.querySelector("#container");
+
+    for( let i = 0; i < 16; i++ ) {
+        for( let j = 0; j < 16; j++ ) {
+
+        const square = document.createElement( "div" );
+
+        square.setAttribute( "class", "square" );
+
+        container.appendChild( square );
+
+        }
+    }
+
+}
+
+createStartingGrid();
